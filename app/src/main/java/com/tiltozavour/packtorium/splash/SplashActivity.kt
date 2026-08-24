5eb40e79d8a5a65fc475ea4b9109dbc9c230fd03
Tiltozavour <soroka144@gmail.com>
2026-08-24 22:21:16 +0300

    Cookies Screen, ui develop

---
+++ app/src/main/java/com/tiltozavour/packtorium/splash/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.tiltozavour.packtorium.splash
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+        val context = applicationContext
+    }
+}
